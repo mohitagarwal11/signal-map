799b2f90fac3e171eafd72b7329f4c4039f507cf
mohitagarwal11 <agarwalmohit1011@gmail.com>
2026-05-27 16:49:15 +0530

    refactor: update database connection string and enhance connection testing

--- backend/src/db/db.py
+++ backend/src/db/db.py
@@ -1,9 +1,23 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.getenv("NEON_CONNECTION_STRING")
+# print("DB URL:", DATABASE_URL)  # add this
 
 engine = create_engine(DATABASE_URL)
+
+# TESTING DB CONNECTION
+with engine.connect() as conn:
+
+    db = conn.execute(
+        text(
+            "SELECT current_database(), COUNT(*) FROM information_schema.tables WHERE table_schema='public'"
+        )
+    ).fetchone()
+    print("Connected to DB:", db[0], "| Public tables:", db[1])
+
+    result = conn.execute(text("SELECT COUNT(*) FROM cell_towers")).scalar()
+    print("Row count:", result)
