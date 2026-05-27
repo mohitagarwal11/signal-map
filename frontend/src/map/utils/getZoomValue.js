d6d391aa019a9fb5f35079d7488101d17b8d932e
mohitagarwal11 <agarwalmohit1011@gmail.com>
2026-05-27 11:39:59 +0530

    refactor: remove unused map utility functions

--- frontend/src/map/utils/getZoomValue.js
+++
@@ -1,7 +0,0 @@
-export function getZoomValue(map) {
-  if (!map) {
-    return null;
-  }
-
-  return Number(map.getZoom().toFixed(2));
-}
--- frontend/src/utils/getCenter.js
+++
@@ -1,7 +0,0 @@
-export function getCenter(map) {
-  const center = map.getCenter();
-
-  // console.log("Center:", center);
-
-  return center;
-}
