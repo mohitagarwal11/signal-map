4066200a47d54fe4e8bf5f59ae4599d025bedf14
mohitagarwal11 <agarwalmohit1011@gmail.com>
2026-06-26 11:00:45 +0530

    data and constants fix

--- frontend/src/constants/initialViewport.js
+++ frontend/src/constants/initialViewport.js
@@ -5,6 +5,8 @@ export const INITIAL_VIEWPORT_BOUNDS = {
   max_lon: 105.33,
 };
 
-export const INITIAL_VIEWPORT_ZOOM = 4;
+export const INITIAL_VIEWPORT_ZOOM = 4.000500641892575;
 
 export const FETCH_DEBOUNCE_MS = 300;
+
+export const INITIAL_MAP_CENTER = { lat: 24.3944, lon: 81.7024 };
